f12350aa173557ffb152c18598ffbd1dc74afbd9
astudillome <projectagility@gmail.com>
2021-05-04 14:11:00 -0600

    updated show artists functionality and put models in models.py

--- app.py
+++ app.py
@@ -135,6 +135,8 @@ def show_venue(venue_id):
         "website": venue.website_link,
         "facebook_link": venue.facebook_link,
         "image_link": venue.image_link,
+        "seeking_talent": venue.seeking_talent,
+        "seeking_description": venue.seeking_description,
         "past_shows": past_shows,
         "upcoming_shows": upcoming_shows,
         "past_shows_count": len(past_shows),
@@ -242,7 +244,42 @@ def show_artist(artist_id):
     # shows the artist page with the given artist_id
     # TODO: replace with real artist data from the artist table, using artist_id-DONE
     artist = Artist.query.filter_by(id=artist_id).first()
-    return render_template('pages/show_artist.html', artist=artist)
+    shows = Show.query.filter_by(artist_id=artist_id).all()
+    
+    past_shows = []
+    upcoming_shows = []
+    for show in shows:
+        venue = Venue.query.filter_by(id=show.venue_id).first()
+        show_detail = {
+            "venue_id": show.venue_id,
+            "venue_name": show.venue_name,
+            "venue_image_link": venue.image_link,
+            "start_time": str(show.start_time)
+        }
+        if (show.start_time < datetime.now()):
+            past_shows.append(show_detail)
+        else:
+            upcoming_shows.append(show_detail)
+
+    artist_data = {
+        "id": artist.id,
+        "name": artist.name,
+        "genres": artist.genres,
+        "city": artist.city,
+        "state": artist.state,
+        "phone": artist.phone,
+        "website": artist.website,
+        "facebook_link": artist.facebook_link,
+        "image_link": artist.image_link,
+        "seeking_description": artist.seeking_description,
+        "seeking_venue": artist.seeking_venue,
+        "seeking_description": artist.seeking_description,
+        "upcoming_shows": upcoming_shows,
+        "upcoming_shows_count": len(upcoming_shows),
+        "past_shows": past_shows,
+        "past_shows_count": len(past_shows)
+    }
+    return render_template('pages/show_artist.html', artist=artist_data)
 
 #  Update
 #  ----------------------------------------------------------------
@@ -408,7 +445,6 @@ def create_show_submission():
         venue_id = request.form.get('venue_id')
         venue_name = Venue.query.filter_by(id=venue_id).first().name
         start_time = request.form.get('start_time')
-        print(artist_name)
 
         show = Show(artist_id=artist_id, artist_name=artist_name, artist_image_link=artist_image_link,
                     venue_id=venue_id, venue_name=venue_name, start_time=start_time)
